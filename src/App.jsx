1f53d34d0733f38c0e866c7779735b292a1bf41b
fawad7998 <fawad7998@gmail.com>
2023-06-24 12:38:30 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+import LoginPage from './loginPage/loginpage';
+function App() {
+  return (
+    <>
+      <LoginPage />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/LoginPageItems/nav.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <div
+      style={{
+        fontfamily: 'Poppins',
+        fontsize: '20px',
+        fontweight: '600',
+        lineheight: '30px',
+        letterspacing: '0em',
+        textalign: 'left',
+        position: 'relative',
+        top: '20px',
+        left: '40px',
+      }}
+    >
+      Your Logo
+    </div>
+  );
+};
+
+export default Nav;
