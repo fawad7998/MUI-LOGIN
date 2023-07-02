078770e86cbbb7caa137311f618e993e7c585c21
fawad7998 <fawad7998@gmail.com>
2023-07-02 13:39:08 -0700

    dsfs

--- src/components/common/signin.jsx
+++ src/components/common/signin.jsx
@@ -1,11 +1,26 @@
+import * as React from 'react';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import {Grid, Stack} from '@mui/material';
+import IconButton from '@mui/material/IconButton';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormControl from '@mui/material/FormControl';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 // import Group from '../assets';
 
 const Signin = () => {
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
   return (
     <Grid
       item
@@ -30,7 +45,7 @@ const Signin = () => {
         component="form"
         sx={{
           paddingLeft: '30px',
-          '& > :not(style)': {width: '25ch', top: '10px'},
+          '& > :not(style)': {width: '25ch', top: '30px'},
         }}
         noValidate
         autoComplete="off"
@@ -44,14 +59,36 @@ const Signin = () => {
       <Box
         component="form"
         sx={{
-          paddingLeft: '30px',
+          paddingLeft: '22px',
+          // marginTop: '5px',
 
           '& > :not(style)': {width: '25ch', top: '40px'},
         }}
         noValidate
         autoComplete="off"
       >
-        <TextField id="outlined-basic" label="Password" variant="outlined" />
+        <FormControl sx={{m: 1, width: '25ch'}} variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-password">
+            Password
+          </InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-password"
+            type={showPassword ? 'text' : 'password'}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Password"
+          />
+        </FormControl>
       </Box>
       <Button
         style={{
